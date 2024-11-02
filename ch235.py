e131c8cccafc57c8117b13d39241d29b83ea815a
Joshua Bluer <jbluer1997@gmail.com>
2024-11-01 22:08:34 -0400

    ch2.3-7

---
+++ ch235.py
@@ -0,0 +1,11 @@
+name = 'Eric'
+print('hello, ' + name + ' how are you?')
+print(name.lower())
+print(name.upper())
+print(name.title())
+print("Forest Gump said 'stupid is as stupid does.'")
+fg = 'Forest Gump'
+ms = "'Life is like a box of choclates'"
+print(fg + ' said, ' + ms)
+gd = '\tBr\ngoodgirl'
+print(gd.strip())
